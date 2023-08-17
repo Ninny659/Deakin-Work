a642798d239daa5cebf393a4165a23892eb3a7f6
Ninny <jordansamcooke@gmail.com>
2023-08-17 16:14:43 +1000

    Updated quick sort

--- QuickSort-Parallel/QuickSortParallel.cpp
+++ QuickSort-Parallel/QuickSortParallel.cpp
@@ -7,26 +7,24 @@
 
 using namespace std;
 
-static int N = 1;
-const int NUM_THREAD = 10;
+static int N = 10;
+const int NUM_THREAD = 4;
 
+pthread_mutex_t lock;
+
+// Arguments to use for both instances of quick sort
 struct ArrayArgs
 {
     int *a1;
     int minLength, maxLength;
 };
 
-void swap(int &a, int &b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
+// Generating a new array and assigning it with random values
 int *unsortedArrayGenerator()
 {
     int *array = new int[N];
     
+    // Creating an array with rnd values between 1-100
     for (int i = 0; i < N; i++)
     {
         array[i] = rand() % 100;
@@ -35,52 +33,92 @@ int *unsortedArrayGenerator()
     return array;
 }
 
+// Timer function
 double timerFunction(std::function<void(void)> timedFunc)
 {
+    // Starting the timer and then running the function. Keeps it very clean. 
     auto start = std::chrono::high_resolution_clock::now();
+
     timedFunc();
+
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
     return elapsed.count();
 }
 
-int partition(int array[], int low, int high)
-{
-    int pivot = array[high];
-    int i = (low - 1);
+int partition(int array[], int minLength, int maxLength) {
+    
+    // Using a mutex on code I want ran in sequential as it might cause errors as multiple threads run this code. 
+    pthread_mutex_lock(&lock);
+    int count = 0;
 
-    for (int j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            swap(array[i], array[j]);
+    // Count the number of elements smaller than or equal to the pivot
+    for (int i = minLength + 1; i <= maxLength; i++) {
+        if (array[i] <= array[minLength]) {
+            count++;
         }
     }
-    swap(array[i + 1], array[high]);
-    return (i + 1);
+
+    // Calculate the pivot's final index
+    int pivotID = minLength + count;
+
+    // Swap the pivot element to its final position
+    swap(array[pivotID], array[minLength]);
+
+    // Initialising variables for traversing the array
+    int i = minLength;
+    int j = maxLength;
+
+    /* Partition the array into two parts: elements less than or equal to pivot on the left,
+    and elements greater than pivot on the right
+    */
+    while (i < pivotID && j > pivotID) {
+        // Find an element on the left side that is greater than the pivot
+        while (array[i] <= array[minLength]) {
+            i++;
+        }
+
+        // Find an element on the right side that is smaller than or equal to the pivot
+        while (array[j] > array[minLength]) {
+            j--;
+        }
+
+        // Swap the elements if i is to the left of pivot and j is to the right of pivot
+        if (i < pivotID && j > pivotID) {
+            swap(array[i++], array[j--]);
+        }
+    }
+
+    pthread_mutex_unlock(&lock);
+    return pivotID;
 }
 
 void *fastort(void *args)
 {
+    // Assigning passed through args to it a local struct args.
     ArrayArgs *fast_task = (ArrayArgs *)args;
 
     int *unsortedArr = fast_task->a1;
     int minLength = fast_task->minLength;
     int maxLength = fast_task->maxLength;
 
+    // Simple edge case to determine if the array exsists and is greater than 1
     if (minLength < maxLength)
     {
-        int pivotIdx = partition(unsortedArr, minLength, maxLength);
+        // Determining the pivot point. 
+        int pivotID = partition(unsortedArr, minLength, maxLength);
 
-        ArrayArgs leftArgs = {unsortedArr, minLength, pivotIdx - 1};
-        ArrayArgs rightArgs = {unsortedArr, pivotIdx + 1, maxLength};
+        // Assigning left and right arguments to pass through the different threads
+        ArrayArgs leftArgs = {unsortedArr, minLength, pivotID - 1};
+        ArrayArgs rightArgs = {unsortedArr, pivotID + 1, maxLength};
 
         pthread_t leftThread, rightThread;
 
+        // Creating and splitting the array in half, running both the left and right sides of the array.
         pthread_create(&leftThread, NULL, fastort, &leftArgs);
         pthread_create(&rightThread, NULL, fastort, &rightArgs);
 
+        // Getting the threads to rejoin to the pool
         pthread_join(leftThread, NULL);
         pthread_join(rightThread, NULL);
     }
@@ -90,44 +128,99 @@ void *fastort(void *args)
 
 double fastortHelper()
 {
-    int *array1 = unsortedArrayGenerator();
-    int *maxSize = new int[N];
-
-    for (int i = 0; i < N; ++i)
-    {
-        maxSize[i] = i + 1;
-    }
+    int *fastArray = unsortedArrayGenerator();
 
     double executionTime_fastification = timerFunction([&]() {
-        ArrayArgs fast_task = {array1, 0, N - 1};
+        // Starting up the arguments for the main thread.
+        ArrayArgs fast_task = {fastArray, 0, N - 1};
         pthread_t mainThread;
+
+        // Running the sorting array on the main thread.
         pthread_create(&mainThread, NULL, fastort, &fast_task);
         pthread_join(mainThread, NULL);
     });
 
-    delete[] array1;
-    delete[] maxSize;
+    // Removing array from memory
+    delete[] fastArray;
 
     return executionTime_fastification;
+}
+
+void *slort(void *args)
+{
+    ArrayArgs *slow_task = (ArrayArgs *)args;
+
+    int *unsortedArr = slow_task->a1;
+    int minLength = slow_task->minLength;
+    int maxLength = slow_task->maxLength;
+
+    // Simple edge case to determine is array exsists and is greater than 1
+    if (minLength < maxLength)
+    {
+        /*
+        Similar to the parallel task, we split the array in half and run the sort on left and right portions of the array.
+        Unlike the parallel task we are recursively running the function and will run all the left operations before running the right operation
+        In theory running this would be slower compared to the parallel task as we are only running one thread compared to multiple threads.  
+        */ 
+        int pivotID = partition(unsortedArr, minLength, maxLength);
+
+        ArrayArgs leftArgs = {unsortedArr, minLength, pivotID - 1};
+        ArrayArgs rightArgs = {unsortedArr, pivotID + 1, maxLength};
+
+        slort(&leftArgs);
+
+        slort(&rightArgs);
+    }
+    
+    return NULL;
+}
+
+double slortHelper()
+{
+    int *slowArray = unsortedArrayGenerator();
+
+    double executionTime_slowification = timerFunction([&]() {
+        // Assiging the needed struct to the sequential function while starting the timer. 
+        ArrayArgs slow_task = {slowArray, 0, N - 1};
+        slort(&slow_task);
+    });
+
+    // Clearing the memory used for the array.
+    delete[] slowArray;
+
+    return executionTime_slowification;
 }
 
 int main()
 {
     const int RUN_TIME = 4;
-    double fastortMean = 0;
 
+    double fastortMean = 0;
+    double slortMean = 0;
+
+    /*
+    A simple function to time the average of RUN_TIME for a multiple of the functions.
+    This allows us to run each function for x (RUN_TIME) amount of timeas and take the average time of the runs.
+    We can then increase the size of the array by multiplying N by z (Where z is an integer).
+    */
     for (int i = 0; i <= 4; i++)
     {
-        for (int j = 0; j < RUN_TIME; j++)
+        fastortMean = 0;
+        slortMean = 0;
+
+        for (int j = 0; j <= RUN_TIME; j++)
         {
+            slortMean += slortHelper();
             fastortMean += fastortHelper();
         }
 
+        slortMean /= RUN_TIME;
         fastortMean /= RUN_TIME;
 
-        std::cout << "--------------------------------------------------------------\n"
-                  << "Execution Time for " << N << " elements (Fastort): " << fastortMean << " seconds (Mean) \n"
-                  << std::endl;
+        // Printing statements to display run time. 
+        std::cout << "--------------------------------------------------------------\n" << "Execution Time for " << N << " elements (Fastort): " << fastortMean << " seconds (Mean) \n" << std::endl;
+        std::cout << "--------------------------------------------------------------\n" << "Execution Time for " << N << " elements (Slort): " << slortMean << " seconds (Mean) \n" << std::endl;
+
         N *= 10;
     }
 
--- Seminar-M2-S3P/VectorAdd.cpp
+++ Seminar-M2-S3P/VectorAdd.cpp
@@ -2,11 +2,23 @@
 #include <cstdlib>
 #include <time.h>
 #include <chrono>
+#include <pthread.h>
 
 using namespace std::chrono;
 using namespace std;
 
-void randomVector(int vector[], int size)
+// const variables we don't want to change in run time
+const int NUM_THREAD = 6;
+static int N = 2;
+
+struct RndVectorRefrence
+{
+    int *v;
+    int startPosition, endPosition;
+};
+
+// OMP implementation ---------------------------------------------------------------------------------------------------------------
+void randomOMPVector(int vector[], int size)
 {
 #pragma omp parallel default(none)
     #pragma omp for
@@ -17,11 +29,8 @@ void randomVector(int vector[], int size)
     }
 }
 
-int main()
+double ompVectorAddition()
 {
-
-    unsigned long size = 100000000;
-
     srand(time(0));
 
     int *v1, *v2, *v3;
@@ -29,21 +38,25 @@ int main()
     // Starting our timer and setting it to the timer_start variable.
     auto start = high_resolution_clock::now();
 
-    // Setting up a vector of vectorSize - int vectorSize
-    v1 = (int *)malloc(size * sizeof(int *));
-    v2 = (int *)malloc(size * sizeof(int *));
-    v3 = (int *)malloc(size * sizeof(int *));
+    // Setting up a vector of N - int N
+    v1 = (int *)malloc(N * sizeof(int *));
+    v2 = (int *)malloc(N * sizeof(int *));
+    v3 = (int *)malloc(N * sizeof(int *));
 
-    randomVector(v1, size);
-    randomVector(v2, size);
+    randomOMPVector(v1, N);
+    randomOMPVector(v2, N);
+
+    int totalV3 = 0;
 
     // Adding vector1 to vector2 to the result of vector3
-#pragma omp parallel default(none) shared(size, v3) private(v1,v2)
+    #pragma omp parallel default(none) shared(N, v3) private(v1, v2) reduction(+:totalV3)
     #pragma omp for schedule(static, chunk)
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < N; i++)
     {
-        #pragma omp atomic
         v3[i] = v1[i] + v2[i];
+        
+        #pragma omp atomic update
+        totalV3 += v3[i];
     }
 
 #pragma omp critical
@@ -51,12 +64,197 @@ int main()
     // Stopping the timer to measure affectivness of our implmentation.
     auto timer_stop = high_resolution_clock::now();
 
-    // Retriving the time value of stop - timer_start together, for result duration.
-    auto duration = duration_cast<microseconds>(timer_stop - start);
+    std::chrono::duration<double> elapsed = timer_stop - start;
+    return elapsed.count();
+}
+}
 
-    cout << "Time taken by function: "
-         << duration.count() << " microseconds" << endl;
+// pThread implementation ---------------------------------------------------------------------------------------------------------------
+struct AddVectorRefrence
+{
+    int *v1, *v2, *v3;
+    int startPosition, endPosition;
+};
+
+void *create_randomVector(void *args)
+{
+    RndVectorRefrence *rnd_vectortaskTask = ((struct RndVectorRefrence *)args);
+
+    for (int i = rnd_vectortaskTask->startPosition; i < rnd_vectortaskTask->endPosition; i++)
+    {
+        // Adding rand value to the vector.
+        rnd_vectortaskTask->v[i] = rand() % 100;
+    }
 
     return 0;
 }
+
+void *adding_vector(void *args)
+{
+    AddVectorRefrence *add_vectorTask = ((struct AddVectorRefrence *)args);
+
+    for (int i = add_vectorTask->startPosition; i < add_vectorTask->endPosition; i++)
+    {
+        // Adding vector1 to vector2 to the result of vector3
+        add_vectorTask->v3[i] = add_vectorTask->v1[i] + add_vectorTask->v2[i];
+    }
+
+    return 0;
+}
+
+double pthreadVectorAddition()
+{
+    pthread_t random_threads[NUM_THREAD];
+    pthread_t add_threads[NUM_THREAD];
+
+    srand(time(0));
+
+    int *v1, *v2, *v3;
+
+    // Starting our timer and setting it to the timer_start variable.
+    auto timer_start = high_resolution_clock::now();
+
+    // Setting up a vector of N - int N
+    v1 = (int *)malloc(N * sizeof(int *));
+    v2 = (int *)malloc(N * sizeof(int *));
+    v3 = (int *)malloc(N * sizeof(int *));
+
+    // Assigning half of our threads to a specific task (We currently have two for loops running at the same time)
+    int Chunk_size = N / NUM_THREAD / 2;
+
+    struct RndVectorRefrence *random_task = (struct RndVectorRefrence *)malloc(sizeof(struct RndVectorRefrence));
+
+    // Creating assigning random values to vector1 and vector2
+    for (size_t i = 0; i < NUM_THREAD / 2; i++)
+    {
+        // Assigning our values to the struct to access in the called function
+        random_task->v = v1;
+        // Defining the start and end position for all of the assigned threads
+        random_task->startPosition = i * Chunk_size;
+        random_task->endPosition = ((i + 1) * Chunk_size) - 1;
+
+        // Creating a thread to run create_randomVector
+        pthread_create(&random_threads[i], NULL, create_randomVector, (void *)random_task);
+    }
+
+    for (size_t i = NUM_THREAD / 2; i < NUM_THREAD; i++)
+    {
+        // See first for loop on explanation.
+        random_task->v = v2;
+        random_task->startPosition = i * Chunk_size;
+        random_task->endPosition = ((i + 1) * Chunk_size) - 1;
+
+        // Creating a thread to run create_randomVector
+        pthread_create(&random_threads[i], NULL, create_randomVector, (void *)random_task);
+    }
+
+    for (size_t i = 0; i < NUM_THREAD; i++)
+    {
+        // Awaiting all of the threads to finish their assigned function and rejoin the thead pool.
+        pthread_join(random_threads[i], NULL);
+    }
+
+    // Assigning all of our threads to complete this task.
+    Chunk_size = N / NUM_THREAD;
+
+    // Iterating and adding the values of V1 and V2 together for result V3
+    for (size_t i = 0; i < NUM_THREAD; i++)
+    {
+        struct AddVectorRefrence *add_task = (struct AddVectorRefrence *)malloc(sizeof(struct AddVectorRefrence));
+
+        // Assigning the values to the previously declared struct to be accessed in the called function
+        add_task->v1 = v1;
+        add_task->v2 = v2;
+        add_task->v3 = v3;
+        add_task->startPosition = i * Chunk_size;
+        add_task->endPosition = ((i + 1) * Chunk_size) - 1;
+
+        // Assigning all of the threads to different chunks of the vector
+        pthread_create(&add_threads[i], NULL, adding_vector, (void *)add_task);
+    }
+
+    for (size_t i = 0; i < NUM_THREAD; i++)
+    {
+        // Awaiting all of the threads to finish their assigned function and rejoin the thead pool.
+        pthread_join(random_threads[i], NULL);
+    }
+
+    // Stopping the timer to measure affectivness of our implmentation.
+    auto timer_stop = high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = timer_stop - timer_start;
+    return elapsed.count();
+}
+
+// Sequential implementation ---------------------------------------------------------------------------------------------------------------
+
+void randomSeqVector(int vector[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        // Adding rand value to the vector.
+        vector[i] = rand() % 100;
+    }
+}
+
+double sequentialAddition()
+{
+    srand(time(0));
+
+    int *v1, *v2, *v3;
+
+    // Starting our timer and setting it to the timer_start variable.
+    auto start = high_resolution_clock::now();
+
+    // Setting up a vector of N - int N
+    v1 = (int *)malloc(N * sizeof(int *));
+    v2 = (int *)malloc(N * sizeof(int *));
+    v3 = (int *)malloc(N * sizeof(int *));
+
+    randomOMPVector(v1, N);
+    randomOMPVector(v2, N);
+
+    // Adding vector1 to vector2 to the result of vector3
+    for (int i = 0; i < N; i++)
+    {
+        v3[i] = v1[i] + v2[i];
+    }
+
+
+    // Stopping the timer to measure affectivness of our implmentation.
+    auto timer_stop = high_resolution_clock::now();
+
+    // Retriving the time value of stop - timer_start together, for result duration.
+    auto duration = duration_cast<microseconds>(timer_stop - start);
+
+    std::chrono::duration<double> elapsed = timer_stop - start;
+    return elapsed.count();
+}
+
+int main()
+{
+    const int RUN_TIME = 5;
+    double seqMean, pthreadMean, ompMean = 0;
+
+    for(int i = 0; i <= 7; i++)
+    {
+        for(int j = 0; j < RUN_TIME; j++){
+            seqMean += sequentialAddition();
+            pthreadMean += pthreadVectorAddition();
+            ompMean += ompVectorAddition();
+        }
+
+        seqMean /= RUN_TIME;
+        pthreadMean /= RUN_TIME;
+        ompMean /= RUN_TIME;
+
+        std::cout << "--------------------------------------------------------------\n"
+        "Execution Time for " << N << " elements (SequentialAddition): " << seqMean << " seconds (Mean) \n" 
+        "Execution Time for " << N << " elements (pThreadVectorAddition): " << pthreadMean << " seconds (Mean) \n" 
+        "Execution Time for " << N << " elements (OMPVectorAddition): " << ompMean << " seconds (Mean) \n" 
+        
+        
+        << std::endl;
+        N *=  10;
+    }
 }
